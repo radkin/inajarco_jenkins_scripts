c2e0ed800dded5dfcadaef3eb533dc9a1759be76
Noel Miller <noeldamonmiller@gmail.com>
2023-12-13 05:44:47 +0000

    build_with_postgresql runs all the tests that are presently working with a container postgresql

--- jenkins1/os_backend%20junit%20tests/pipeline.groovy
+++ jenkins1/os_backend%20junit%20tests/pipeline.groovy
@@ -1,22 +1,37 @@
 pipeline {
     agent any
-
+    
+    environment {
+        POSTGRES_USER = "testing"
+        POSTGRES_DB = "testing_db"
+    }
+    
+    parameters {
+      credentials credentialType: 'org.jenkinsci.plugins.plaincredentials.impl.StringCredentialsImpl', defaultValue: 'testingPassword', description: 'credentials for connecting to our docker postgresql instance spun up for testing', name: 'POSTGRES_PASSWORD', required: true
+    }
+    
     stages {
         stage('Checkout') {
             steps {
                 git branch: '2023-12-06-tests', url: 'https://github.com/radkin/os_backend.git'
             }
         }
+        
+        stage('Postgresql') {
+            steps {
+                sh 'docker-compose up -d dbpostgresql'
+            }
+        }
 
         stage('Build') {
             steps {
-                sh 'mvn clean package'
+              sh 'mvn clean package'
             }
         }
 
         stage('Test') {
             steps {
-                sh 'mvn clean test'
+              sh 'mvn clean test'
             }
         }
     }
@@ -25,4 +40,3 @@ pipeline {
         jdk 'jdk19'
     }
 }
-
